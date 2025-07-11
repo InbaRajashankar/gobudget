2c292aded2d1525c0d162829b1821c61d3cdd6d1
Inba <rkrishinba@gmail.com>
2025-07-10 20:53:13 -0500

    playing around with sqlite

--- main.go
+++ main.go
@@ -1,19 +1,19 @@
-package main
+// package main
 
-import (
-	"fmt"
-	"os"
-)
+// import (
+// 	"fmt"
+// 	"os"
+// )
 
-func main() {
-	for i := 0; i < len(os.Args); i++ {
-		fmt.Println(i, os.Args[i])
-	}
-	if len(os.Args) < 2 {
-		fmt.Println("Please provide an argument")
-		return
-	}
+// // func main() {
+// // 	for i := 0; i < len(os.Args); i++ {
+// // 		fmt.Println(i, os.Args[i])
+// // 	}
+// // 	if len(os.Args) < 2 {
+// // 		fmt.Println("Please provide an argument")
+// // 		return
+// // 	}
 
-	input := os.Args[1]
-	fmt.Println("You entered: ", input)
-}
+// // 	input := os.Args[1]
+// // 	fmt.Println("You entered: ", input)
+// // }
