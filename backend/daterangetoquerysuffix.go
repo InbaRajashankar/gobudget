3df44b55817bb496a5629a9f1c378c14167a81b2
Inba <rkrishinba@gmail.com>
2025-07-13 14:38:00 -0500

    renamed grab_queries.go to grab.go, renamed DateRangeToQuery() and moved to own file

--- backend/daterangetoquerysuffix.go
+++ backend/daterangetoquerysuffix.go
@@ -1,89 +1,15 @@
 package backend
 
 import (
-	"database/sql"
 	"errors"
-	"log"
 	"strconv"
 	"strings"
 
 	"github.com/InbaRajashankar/gobudget/utils"
-
-	_ "github.com/mattn/go-sqlite3"
 )
 
-const db_path string = "./test.db"
-
-// SelectAll selects all entries from the db.
-func SelectAll() *sql.Rows {
-	db, err := sql.Open("sqlite3", db_path)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
-
-	rows, err := db.Query("SELECT * FROM expenses")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return rows
-}
-
-// SelectFilter selects all items from the db with some filter.
-func SelectFilter(args map[string]string) (*sql.Rows, error) {
-	db, err := sql.Open("sqlite3", db_path)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
-
-	query := "SELECT * FROM expenses"
-	conditions_applied := 0 // in case an AND is needed in between multiple BETWEEN statements
-
-	// filter by time
-	date_range, ok := args["-d"]
-	if ok {
-		suffix, err := DateRangeToQueryString(date_range)
-		if err != nil {
-			return nil, err
-		}
-		query += suffix
-		conditions_applied++
-	}
-
-	// filter by price
-	price_range, ok := args["-p"]
-	price_arr := strings.Split(price_range, ",")
-	if ok {
-		if len(price_arr) != 2 || price_arr[0] > price_arr[1] {
-			return nil, errors.New("invalid price range")
-		}
-		if conditions_applied > 0 {
-			query += " AND price BETWEEN " + price_arr[0] + " AND " + price_arr[1]
-		} else {
-			query += " WHERE price BETWEEN " + price_arr[0] + " AND " + price_arr[1]
-		}
-	}
-
-	// top n rows
-	n_rows, ok := args["-n"]
-	if ok {
-		query += " LIMIT " + n_rows
-	}
-
-	log.Println(query)
-
-	rows, err := db.Query(query)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return rows, nil
-}
-
-// M/D/Y,M/D/Y ->
-func DateRangeToQueryString(range_str string) (string, error) {
+// Changes a date range in the format M/D/Y,M/D/Y to an SQLite WHERE statement
+func DateRangeToQuerySuffix(range_str string) (string, error) {
 	// Process string into dates, validation
 	times := strings.Split(range_str, ",")
 	if len(times) != 2 {
@@ -151,24 +77,4 @@ func DateRangeToQueryString(range_str string) (string, error) {
 	}
 
 	return query, nil
-}
-
-func PrintRows(rows *sql.Rows) {
-	for rows.Next() {
-		var id int
-		var day int
-		var month int
-		var year int
-		var name string
-		var price float64
-		var tag string
-		err := rows.Scan(&id, &day, &month, &year, &name, &price, &tag)
-		if err != nil {
-			log.Fatal(err)
-		}
-		log.Printf("%d %d %d %d %s %.2f %s", id, day, month, year, name, price, tag)
-	}
-	if err := rows.Err(); err != nil {
-		log.Fatal(err)
-	}
 }
