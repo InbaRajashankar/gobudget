47ff96c0e431204955c40872f893034462ad74e3
Inba <rkrishinba@gmail.com>
2025-07-19 16:41:23 -0500

    added handler for grab command, updated some checks for args passed in

--- backend/grab.go
+++ backend/grab.go
@@ -3,7 +3,9 @@ package backend
 import (
 	"database/sql"
 	"errors"
+	"fmt"
 	"log"
+	"strconv"
 	"strings"
 
 	_ "github.com/mattn/go-sqlite3"
@@ -52,9 +54,18 @@ func GrabFilter(args map[string]string) (*sql.Rows, error) {
 	// filter by price
 	price_range, ok := args["-p"]
 	price_arr := strings.Split(price_range, ",")
+	first_price, err := strconv.Atoi(price_arr[0])
+	if err != nil {
+		return nil, errors.New("invalid first price, cannot convert to integer")
+	}
+	second_price, err := strconv.Atoi(price_arr[1])
+	if err != nil {
+		return nil, errors.New("invalid second price, cannot convert to integer")
+	}
 	if ok {
-		if len(price_arr) != 2 || price_arr[0] > price_arr[1] {
-			return nil, errors.New("invalid price range")
+		if len(price_arr) != 2 || first_price > second_price {
+			error_string := fmt.Sprintf("invalid price range %s %s", price_arr[0], price_arr[1])
+			return nil, errors.New(error_string)
 		}
 		if conditions_applied > 0 {
 			query += " AND price BETWEEN " + price_arr[0] + " AND " + price_arr[1]
@@ -66,6 +77,10 @@ func GrabFilter(args map[string]string) (*sql.Rows, error) {
 	// top n rows
 	n_rows, ok := args["-n"]
 	if ok {
+		_, err = strconv.Atoi(n_rows) // check if n_rows is an integer
+		if err != nil {
+			return nil, errors.New("invalid number of entries")
+		}
 		query += " LIMIT " + n_rows
 	}
 
--- frontend/frontend.go
+++ frontend/frontend.go
@@ -2,10 +2,14 @@ package frontend
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"log"
 	"os"
+	"slices"
 	"strings"
+
+	"github.com/InbaRajashankar/gobudget/backend"
 )
 
 func InteractionLoop() {
@@ -23,12 +27,20 @@ func InteractionLoop() {
 
 		switch command_arr[0] {
 		case "quit", "q":
-			fmt.Println("Goodbye :3")
+			fmt.Println("Goodbye...")
 			os.Exit(0)
 		case "help", "h":
 			log.Fatal("help has not been implemented yet :p")
+		case "clear", "c":
+			log.Fatal("clear has not been implemented yet :p")
 		case "grab":
-			fmt.Println("grab")
+			acceptable_args := []string{"-d", "-p", "-n", "-csv"}
+			args, err := CreateArgMap(command_arr, acceptable_args)
+			if err != nil {
+				fmt.Println(err.Error())
+			} else {
+				HandleGrab(args)
+			}
 		case "grabsum":
 			fmt.Println("grabsum")
 		case "graph":
@@ -39,5 +51,38 @@ func InteractionLoop() {
 			fmt.Println("Invalid command, please enter h for help.")
 		}
 
+	}
+}
+
+// CreateArgMap creates a map storing [arg]value, only including specified flags
+func CreateArgMap(arr []string, acceptable_args []string) (map[string]string, error) {
+	args := make(map[string]string)
+	for ind, val := range arr {
+		if slices.Contains(acceptable_args, val) {
+			_, already_in_map := args[val]
+			if already_in_map {
+				return nil, errors.New("invalid input: duplicate tags")
+			}
+			if ind+1 >= len(arr) {
+				return nil, errors.New("invalid input: no value for final arg")
+			}
+			args[val] = arr[ind+1]
+		}
+	}
+	return args, nil
+}
+
+// HandleGrab is the handler if the command is "grab".
+func HandleGrab(args map[string]string) {
+	if len(args) == 0 {
+		rows := backend.GrabAll()
+		backend.PrintRows(rows)
+	} else {
+		rows, err := backend.GrabFilter(args)
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			backend.PrintRows(rows)
+		}
 	}
 }
